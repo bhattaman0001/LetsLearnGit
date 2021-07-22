a9db834ceaaca9a7ba6956324c02184c171c81da
Aman Bhatt <dheerajbhatt301@gmail.com>
2021-07-22 14:03:51 +0530

    initial commit

---
+++ .dist/sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include <math.h>
+#include <string.h>
+#include <string>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+int main()
+{   
+    int a,b;
+    cin>>a>>b;
+
+    int sum = a+b;
+    cout<<sum<<endl;
+    return 0;
+}
