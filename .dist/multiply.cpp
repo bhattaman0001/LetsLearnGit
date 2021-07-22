57a92acc121be68c56b473035c8e2dad973e5142
Aman Bhatt <dheerajbhatt301@gmail.com>
2021-07-22 14:29:09 +0530

    added multiply.cpp

---
+++ .dist/multiply.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include <math.h>
+#include <string.h>
+#include <string>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+int main()
+{   
+    int a,b;
+    cin>>a>>b;
+
+    int m = a*b;
+    cout<<m;
+    return 0;
+}
